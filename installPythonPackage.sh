3f43f28280f9082079414431de1ab7caa3d0710f
Marcel Vonlanthen <marcel.vonlanthen@qbis.ch>
2018-07-17 12:01:45 +0200

    Initial commit

---
+++ installPythonPackage.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+# Install extra Python package on the Anaconda installation in MS Azure
+# HDInsight cluster. Run this script as "Script Action".
+
+# example on env py35
+#sudo /usr/bin/anaconda/bin/conda install -c plotly plotly --name py35
